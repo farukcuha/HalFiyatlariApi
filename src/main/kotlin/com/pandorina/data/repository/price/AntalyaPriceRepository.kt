d2a5222e2c10366a12a4cb1a3b71c9ac63619778
ahmetfarukcuha <ahmetfarukcuha@hotmail.com>
2022-11-02 23:11:13 +0300

    fixes in antalya price repository

--- src/main/kotlin/com/pandorina/data/repository/price/AntalyaPriceRepository.kt
+++ src/main/kotlin/com/pandorina/data/repository/price/AntalyaPriceRepository.kt
@@ -7,31 +7,29 @@ import com.pandorina.domain.model.SyncResponse
 class AntalyaPriceRepository: BasePriceRepository() {
     companion object{
         const val cityId = "antalya"
-        const val srcUrl = "https://www.guncelfiyatlari.com/antalya-hal-fiyatlari"
+        const val srcUrl = "https://www.batiakdeniztv.com/antalya-hal-fiyatlari-s39.html"
     }
     override suspend fun syncPrices(): SyncResponse? {
         return HtmlFetcher<List<JsoupPrice>>(
             url = srcUrl,
             parseHtml = { jsoup ->
                 mutableListOf<JsoupPrice>().apply {
-                    val elements = jsoup.select("table > tbody > tr")
-                    val date = elements[0].select("p").text()
-                    for (i in 2 until elements.size){
+                    val elements = jsoup.select("table[summary=Kumluca Hal Fiyatları] tr")
+                    val date = jsoup.select("tr > td > p > span[style=font-size:48px;] > b").text()
+                    for (i in 0 until elements.size){
                         val row = elements[i].select("td")
-                        val name = row.getOrNull(0)?.text()
-                        val icon = row.select("img").attr("data-layzr")
-                        val measure = row.getOrNull(1)?.text()
-                        val lowPrice = row.getOrNull(2)?.text()
-                        val highPrice = row.getOrNull(3)?.text()
+                        val name = row.getOrNull(1)?.text()
+                        val price = row.getOrNull(2)?.text()
+                        if (name?.isNotEmpty() == true && price?.isNotEmpty() == true)
                         add(
                             JsoupPrice(
                                 cityId = cityId,
                                 priceDate = date,
                                 name = name,
-                                icon = icon,
-                                measure = measure,
-                                pricePrimary = lowPrice,
-                                priceSecondary = highPrice
+                                icon = null,
+                                measure = "Kilogram",
+                                pricePrimary = price,
+                                priceSecondary = null
                             )
                         )
                     }
